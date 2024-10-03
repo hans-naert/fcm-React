3aee4cbd40d132c1c8f2b38b5b3adf77c125653d
Hans Naert <hans.naert@vives.be>
2024-10-03 19:29:20 +0200

    Add Firebase Cloud Messaging

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFBZWbM_Oe3-9h_n2cz2eIgQpc9rlR5XA",
+  authDomain: "fcm-les.firebaseapp.com",
+  projectId: "fcm-les",
+  storageBucket: "fcm-les.appspot.com",
+  messagingSenderId: "446774523019",
+  appId: "1:446774523019:web:4468476e98f0492bf5908e",
+  measurementId: "G-G5HJGL3V5S"
+};
+
+
+export const vapidKey = "BJjSZo70cBJXB6WLTGDqByNNV1jYYrtfr8Uynhh_hox44OArPNemqfPqHBYMHgZTyu2dP3WhUwFuN_hdsbOudDc";
--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { firebaseConfig, vapidKey } from './firebaseConfig';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
+// Add the public key generated from the console here.
+
+(function requestPermission() {
+  console.log('Requesting permission...');
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+    }
+  }
+)})();
+
+getToken(messaging, {vapidKey: vapidKey}).then((currentToken) => {
+  console.log('Token: ', currentToken)});
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker
+//   `messaging.onBackgroundMessage` handler.
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
